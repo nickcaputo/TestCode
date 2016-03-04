1020ce070f117290380e0721abf21fa7498ea24e
npoCaputo <nicholasCaputo12@Augustana.edu>
2016-03-04 12:07:54 -0600

    Added two methods to determine if a year is a leap year (divisible by 4 except if it is both divisible by 100 and not divisible by 400) and also a method that determines if a given date is legal.

--- PlayTest/src/main_threads/PlayTest.java
+++ PlayTest/src/main_threads/PlayTest.java
@@ -81,6 +81,8 @@ public class PlayTest {
 			}
 		}
 
+//		 Write.writeLine(isALegalDate(2015, 02, 28));
+
 		// Write.writeLine(getValue("TRADINGFEW", "LGXWEV"));
 
 		// Write.writeLine(serializeContact(contacts, "qs", "test request",
@@ -200,6 +202,32 @@ public class PlayTest {
 		} catch (SQLException err) {
 			err.printStackTrace();
 			return null;
+		}
+	}
+
+	private static boolean isALeapYear(int year) {
+		return (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+	}
+
+	private static boolean isALegalDate(int year, int month, int day) {
+		// check date legality
+		if ((month <= 7 && month % 2 == 0) || (month >= 8 && month % 2 == 1)) {
+			if (month == 2) {
+				if (isALeapYear(year)) {
+					System.out.println("Is a february in leap year");
+					return day <= 29;
+				} else {
+					System.out.println("Is a feburary in a normal year");
+					return day <= 28;
+				}
+			}
+
+			System.out.println("Is an even month before July (but not February) or an odd month after August");
+			return day <= 30;
+		} else {
+
+			System.out.println("Is an even month after August, or odd month before july");
+			return day <= 31;
 		}
 	}
 
